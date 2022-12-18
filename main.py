486d026ddd88db44b3bd63cbd768f66495b6556f
Paul Harris <paul@trickbooter.com>
2022-12-18 19:48:54 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 def conn_str():
-  return "Server=10.10.132.12;Database=myDataBase;User Id=myUsername;Password=myComplexPass;"
+    '''my docstring'''
+    return "Server=10.10.132.12;Database=myDataBase;User Id=myUsername;Password=myComplexPass;"
